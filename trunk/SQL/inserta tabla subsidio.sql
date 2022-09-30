336479d65571bbb7479fcf541b6ccaa855a7387f
Manuel Cruz Hernandez <mcmanuelster@gmail.com>
2022-09-30 14:45:30 -0500

    insert de catalogos hr

---
+++ trunk/SQL/inserta tabla subsidio.sql
@@ -0,0 +1,50 @@
+INSERT INTO tabla_subsidio values (1,2021,1,0.01,872.85,200.85);
+INSERT INTO tabla_subsidio values (2,2021,1,872.86,1309.2,200.7);
+INSERT INTO tabla_subsidio values (3,2021,1,1309.21,1713.6,200.7);
+INSERT INTO tabla_subsidio values (4,2021,1,1713.61,1745.7,193.8);
+INSERT INTO tabla_subsidio values (5,2021,1,1745.71,2193.75,188.7);
+INSERT INTO tabla_subsidio values (6,2021,1,2193.76,2327.55,174.75);
+INSERT INTO tabla_subsidio values (7,2021,1,2327.56,2632.65,160.35);
+INSERT INTO tabla_subsidio values (8,2021,1,2632.66,3071.4,145.35);
+INSERT INTO tabla_subsidio values (9,2021,1,3071.41,3510.15,125.1);
+INSERT INTO tabla_subsidio values (10,2021,1,3510.16,3642.6,107.4);
+INSERT INTO tabla_subsidio values (11,2021,1,3642.61,999999.99,0);
+
+INSERT INTO tabla_subsidio values (12,2021,2,0.01,1768.96,407.02);
+INSERT INTO tabla_subsidio values (13,2021,2,1768.97,2653.38,406.83);
+INSERT INTO tabla_subsidio values (14,2021,2,2653.39,3472.84,406.62);
+INSERT INTO tabla_subsidio values (15,2021,2,3472.85,3537.87,392.77);
+INSERT INTO tabla_subsidio values (16,2021,2,3537.88,4446.15,382.46);
+INSERT INTO tabla_subsidio values (17,2021,2,4446.16,4717.18,354.23);
+INSERT INTO tabla_subsidio values (18,2021,2,4717.19,5335.42,324.87);
+INSERT INTO tabla_subsidio values (19,2021,2,5335.43,6224.67,294.63);
+INSERT INTO tabla_subsidio values (20,2021,2,6224.68,7113.9,253.54);
+INSERT INTO tabla_subsidio values (21,2021,2,7113.91,7382.33,217.61);
+INSERT INTO tabla_subsidio values (22,2021,2,7382.34,999999.99,0);
+
+
+INSERT INTO tabla_subsidio values (23,2022,1,0.01,872.85,200.85);
+INSERT INTO tabla_subsidio values (24,2022,1,872.86,1309.2,200.7);
+INSERT INTO tabla_subsidio values (25,2022,1,1309.21,1713.6,200.7);
+INSERT INTO tabla_subsidio values (26,2022,1,1713.61,1745.7,193.8);
+INSERT INTO tabla_subsidio values (27,2022,1,1745.71,2193.75,188.7);
+INSERT INTO tabla_subsidio values (28,2022,1,2193.76,2327.55,174.75);
+INSERT INTO tabla_subsidio values (29,2022,1,2327.56,2632.65,160.35);
+INSERT INTO tabla_subsidio values (30,2022,1,2632.66,3071.4,145.35);
+INSERT INTO tabla_subsidio values (31,2022,1,3071.41,3510.15,125.1);
+INSERT INTO tabla_subsidio values (32,2022,1,3510.16,3642.6,107.4);
+INSERT INTO tabla_subsidio values (33,2022,1,3642.61,999999.99,0);
+
+
+INSERT INTO tabla_subsidio values (34,2022,2,0.01,1768.96,407.02);
+INSERT INTO tabla_subsidio values (35,2022,2,1768.97,2653.38,406.83);
+INSERT INTO tabla_subsidio values (36,2022,2,2653.39,3472.84,406.62);
+INSERT INTO tabla_subsidio values (37,2022,2,3472.85,3537.87,392.77);
+INSERT INTO tabla_subsidio values (38,2022,2,3537.88,4446.15,382.46);
+INSERT INTO tabla_subsidio values (39,2022,2,4446.16,4717.18,354.23);
+INSERT INTO tabla_subsidio values (40,2022,2,4717.19,5335.42,324.87);
+INSERT INTO tabla_subsidio values (41,2022,2,5335.43,6224.67,294.63);
+INSERT INTO tabla_subsidio values (42,2022,2,6224.68,7113.9,253.54);
+INSERT INTO tabla_subsidio values (43,2022,2,7113.91,7382.33,217.61);
+INSERT INTO tabla_subsidio values (44,2022,2,7382.34,999999.99,0);
+
