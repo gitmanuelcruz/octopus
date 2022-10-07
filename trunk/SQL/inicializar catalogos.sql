5e7fcb89f1dc385733c0659946bcf764535ca5ff
Manuel Cruz Hernandez <mcmanuelster@gmail.com>
2022-10-07 15:31:53 -0500

    ...

--- trunk/SQL/inicializar catalogos.sql
+++ trunk/SQL/inicializar catalogos.sql
@@ -1,3 +1,9 @@
+/**
+inserta catalogos de Departamentos
+*/
 INSERT INTO departamentos(id_departamento,id_departamento_padre,clave_departamento,nombre_departamento,es_centro_costo,departamento_activo)
-VALUES(-1,NULL,'000','Not defined',0,0);
-
+VALUES(-1,NULL,'000','Not defined',0,0)
+;
+/**
+inserta catalogo de Niveles del perfil
+*/
