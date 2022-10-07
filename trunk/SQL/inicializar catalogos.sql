9388dadb4573ca2dabbe95993a0605c1220250b5
Manuel Cruz Hernandez <mcmanuelster@gmail.com>
2022-10-07 15:26:27 -0500

    .catalogos iniciales para poder arrancar desde cualquier sistema

---
+++ trunk/SQL/inicializar catalogos.sql
@@ -0,0 +1,3 @@
+INSERT INTO departamentos(id_departamento,id_departamento_padre,clave_departamento,nombre_departamento,es_centro_costo,departamento_activo)
+VALUES(-1,NULL,'000','Not defined',0,0);
+
